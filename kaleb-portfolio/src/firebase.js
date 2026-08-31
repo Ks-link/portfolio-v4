ffc2822d9a1fcef120581924862ed399b809fc78
Kaleb Link <kaleb.s.link@gmail.com>
2026-08-31 10:41:46 -0700

    Refactor Firebase configuration to use environment variables
    - Updated firebase.js to replace hardcoded configuration values with environment variables for improved security and flexibility.
    - Ensured sensitive information is not exposed in the codebase.

--- kaleb-portfolio/src/firebase.js
+++ kaleb-portfolio/src/firebase.js
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCz4YaG8GBsH4l6s8jxlGSMe8Sn_NT6png',
-  authDomain: 'kaleblink-portfolio-v4.firebaseapp.com',
-  projectId: 'kaleblink-portfolio-v4',
-  storageBucket: 'kaleblink-portfolio-v4.firebasestorage.app',
-  messagingSenderId: '85383932283',
-  appId: '1:85383932283:web:bee07e7fd7fcbae22eb5de',
-  databaseURL: 'https://kaleblink-portfolio-v4-default-rtdb.firebaseio.com',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 }
 
 const app = initializeApp(firebaseConfig)
